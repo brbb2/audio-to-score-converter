c5f10cab71f1346504183f04a325952b1ccd7344
brbb2 <brbb2@cam.ac.uk>
2019-11-04 19:26:30 +0000

    Modularise code

--- test-neural-network.py
+++ test-neural-network.py
@@ -1,8 +1,7 @@
-# import pandas as pd
-import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
-# from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import numpy as np
+import random
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.models import model_from_json
@@ -22,62 +21,73 @@ def show_image(image):
     plt.show()
 
 
-def train_and_save_model(name, x_train, y_train):
-    # x_train, y_train, x_test, y_test = get_data()
-    _, _, x_test, y_test = get_data()
-
-    # define model
+def define_model_test2():
     model = Sequential()
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dense(128, activation='relu'))
     model.add(Dense(10, activation='softmax'))
+    return model
 
-    # compile model
+
+def train_model(name, x_train, y_train, epochs=3, saving=True):
+
+    model = define_model_test2()
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=epochs)
 
-    # fit model
-    model.fit(x_train, y_train, epochs=3)
-
-    # save model
-    # model.save(f'scratch-models/{name}.model')
-
-    # serialize model to JSON
-    model_json = model.to_json()
-    with open(f'scratch-models/{name}.json', 'w') as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights(f'scratch-models/{name}.h5')
-    '''
-    # evaluate model
-    val_loss, val_acc = model.evaluate(x_test, y_test)
-    print(f'\nval_loss = {val_loss}\nval_acc = {val_acc}')
-
-    # use model to make predictions
-    predictions = model.predict([x_test])
-    print(np.argmax(predictions[0]))
-    '''
+    # save model to JSON and save weights to HDF5
+    if saving:
+        model_json = model.to_json()
+        with open(f'scratch-models/{name}.json', 'w') as json_file:
+            json_file.write(model_json)
+        model.save_weights(f'scratch-models/{name}.h5')
 
 
-def evaluate_model(model_name, x_test, y_test):
-    # model = tf.keras.models.load_model(f'scratch-models/{model_name}.model')
-    # val_loss, val_acc = model.evaluate(x_test, y_test)
-    # print(f'val_loss = {val_loss}\nval_acc{val_acc}')
+def load_model(model_name):
     json_file = open(f'scratch-models/{model_name}.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
     loaded_model.load_weights(f'scratch-models/{model_name}.h5')
-    predictions = loaded_model.predict([x_test])
-    print(np.argmax(predictions[0]))
+    return loaded_model
+
+
+def get_predictions(model_name, test_set):
+    model = load_model(model_name)
+    return model.predict([test_set])
+
+
+def display_predictions(predictions, index=False):
+    if index:
+        return [np.argmax(predictions[x]) for x in range(len(predictions))]
+    else:
+        return [predictions[x][np.argmax(predictions[x])] for x in range(len(predictions))]
+
+
+def evaluate_model(model_name, x_test, y_test):
+    model = load_model(model_name)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    val_loss, val_acc = model.evaluate(x_test, y_test)
+    print(f'\nval_loss = {val_loss}\nval_acc  = {val_acc}')
+
+
+def generate_randint_array(i, j):
+    test_array = np.ones([i, j])
+    for a in range(i):
+        for b in range(j):
+            test_array[a, b] = random.randint(0, 10)
+    print(test_array)
+    print(display_predictions(test_array))
+    print(display_predictions(test_array, index=True))
 
 
 def main():
-    x_train, y_train, x_test, y_test = get_data()
-    # train_and_save_model('test2', x_train, y_train)
-    evaluate_model('test2', x_test, y_test)
-    # show_image(x_test[1])
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # train_model('test2', x_train, y_train)
+    print(display_predictions(get_predictions('test2', x_test), index=True))
+    # evaluate_model('test2', x_test, y_test)
+    # show_image(x_test[0])
 
 
 if __name__ == "__main__":
