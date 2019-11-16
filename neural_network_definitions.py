6dc9c8250dcc97ff03217a73ae12b8c54d69c34b
brbb2 <brbb2@cam.ac.uk>
2019-11-16 22:05:26 +0000

    Add input_shape argument to first layer

--- neural_network_definitions.py
+++ neural_network_definitions.py
@@ -2,11 +2,11 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv1D, MaxPool1D
 
 
-def get_model_baseline():
+def get_model_baseline(x_train_shape):
 
     model = Sequential()
 
-    model.add(Conv1D(filters=1024, kernel_size=4))
+    model.add(Conv1D(filters=1024, kernel_size=4, input_shape=x_train_shape[1:]))
     model.add(MaxPool1D(pool_size=2))
 
     model.add(Conv1D(filters=512, kernel_size=16))
