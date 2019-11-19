377ce1ee5f8ccd971db3895408defd9181a3a026
brbb2 <brbb2@cam.ac.uk>
2019-11-19 23:28:05 +0000

    Correct baseline model definition

--- neural_network_definitions.py
+++ neural_network_definitions.py
@@ -1,20 +1,22 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv1D, MaxPool1D
+from keras.layers import Dense, Conv1D, MaxPool1D, Flatten
 
 
-def get_model_baseline(x_train_shape):
+def get_model_baseline(x_train_shape, printing=False):
 
     model = Sequential()
 
-    model.add(Conv1D(filters=1024, kernel_size=4, input_shape=x_train_shape[1:]))
-    model.add(MaxPool1D(pool_size=2))
+    model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=32))
 
-    model.add(Conv1D(filters=512, kernel_size=16))
-    model.add(MaxPool1D(pool_size=2))
+    model.add(Conv1D(filters=8, kernel_size=64))
+    model.add(MaxPool1D(pool_size=16))
 
-    model.add(Conv1D(filters=178, kernel_size=64))  # (2 * 89) output filters
-    model.add(MaxPool1D(pool_size=2))
-
+    model.add(Flatten())
     model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
 
     return model
