d026c4182e932e5aaf6c44fa10ca050a99001197
brbb2 <brbb2@cam.ac.uk>
2019-11-29 00:06:04 +0000

    Add some alternative model definitions

--- neural_network_definitions.py
+++ neural_network_definitions.py
@@ -22,3 +22,135 @@ def get_model_baseline(x_train_shape, printing=False):
             print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
 
     return model
+
+
+def get_model_3(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Conv1D(filters=8, kernel_size=64, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=16))
+
+    model.add(Conv1D(filters=8, kernel_size=128))
+    model.add(MaxPool1D(pool_size=16))
+
+    model.add(Flatten())
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        # print(f'feature shape: {str(feature_shape): >18}')
+        # print(f' target shape: {str(target_shape): >18}\n')
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def get_model_4(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Conv1D(filters=8, kernel_size=64, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=16))
+
+    model.add(Conv1D(filters=8, kernel_size=256))
+    model.add(MaxPool1D(pool_size=16))
+
+    model.add(Flatten())
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        # print(f'feature shape: {str(feature_shape): >18}')
+        # print(f' target shape: {str(target_shape): >18}\n')
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def get_model_5(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Conv1D(filters=8, kernel_size=2048, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=16))
+
+    model.add(Conv1D(filters=8, kernel_size=64))
+    model.add(MaxPool1D(pool_size=16))
+
+    model.add(Flatten())
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        # print(f'feature shape: {str(feature_shape): >18}')
+        # print(f' target shape: {str(target_shape): >18}\n')
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def get_model_6(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Conv1D(filters=8, kernel_size=16, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=4))
+
+    model.add(Conv1D(filters=8, kernel_size=32))
+    model.add(MaxPool1D(pool_size=8))
+
+    model.add(Conv1D(filters=8, kernel_size=64))
+    model.add(MaxPool1D(pool_size=8))
+
+    model.add(Flatten())
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        # print(f'feature shape: {str(feature_shape): >18}')
+        # print(f' target shape: {str(target_shape): >18}\n')
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def get_model_7(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Conv1D(filters=8, kernel_size=16, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=2))
+
+    model.add(Conv1D(filters=8, kernel_size=32))
+    model.add(MaxPool1D(pool_size=4))
+
+    model.add(Conv1D(filters=8, kernel_size=64))
+    model.add(MaxPool1D(pool_size=4))
+
+    model.add(Conv1D(filters=8, kernel_size=128))
+    model.add(MaxPool1D(pool_size=8))
+
+    model.add(Flatten())
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        # print(f'feature shape: {str(feature_shape): >18}')
+        # print(f' target shape: {str(target_shape): >18}\n')
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def main():
+    get_model_7(x_train_shape=(None, 8193, 1), printing=True)
+
+
+if __name__ == '__main__':
+    main()
