c347b5e5914eda7879bebd06c074f17211fc0500
brbb2 <brbb2@cam.ac.uk>
2019-11-29 00:06:52 +0000

    Rename some functions

--- evaluator.py
+++ evaluator.py
@@ -38,7 +38,7 @@ def get_maxima_times(single_frequency_spectrum, times, threshold, printing=False
     return np.array(maxima_times)
 
 
-def get_all_maxima_times(spectrum, times, threshold, printing=False):
+def get_maxima_times_for_all_pitches(spectrum, times, threshold, printing=False):
     note_predictions = np.empty(len(spectrum), dtype=object)
     for i in range(len(spectrum)):
         single_frequency_spectrum = spectrum[i]
@@ -48,10 +48,10 @@ def get_all_maxima_times(spectrum, times, threshold, printing=False):
 
 
 def get_note_predictions(pitches, spectrum, times, threshold):
-    all_maxima_times = get_all_maxima_times(spectrum, times, threshold)
+    maxima_times_for_all_pitches = get_maxima_times_for_all_pitches(spectrum, times, threshold)
     note_predictions = np.empty(len(pitches), dtype=object)
     for i in range(len(pitches)):
-        note_predictions[i] = (pitches[i], all_maxima_times[i])
+        note_predictions[i] = (pitches[i], maxima_times_for_all_pitches[i])
     return note_predictions
 
 
@@ -83,7 +83,7 @@ def plot_smoothed(t, spectrum):
 
 def get_pitch_probabilities(wav_name, model, number_of_pitches=89, plotting=False, printing=False):
 
-    spectrum, _, times, _ = get_spectrogram(f'scratch-wav-files/{wav_name}.wav')
+    spectrum, _, times, _ = get_spectrogram(f'wav_files/{wav_name}.wav')
     all_pitch_activations = np.empty(number_of_pitches, dtype=object)
 
     if plotting:
@@ -122,18 +122,18 @@ def get_pitch_probabilities(wav_name, model, number_of_pitches=89, plotting=Fals
 
 
 def main():
-    spectrum, _, t, _ = get_spectrogram('scratch-wav-files/C5.wav')
-    number_of_pitches = 7
-    number_of_time_steps = 10
-    t = np.linspace(0, 2.5, number_of_time_steps)
-    pitches = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    # spectrum, _, t, _ = get_spectrogram('scratch-wav-files/C5.wav')
+    # number_of_pitches = 7
+    # number_of_time_steps = 10
+    # t = np.linspace(0, 2.5, number_of_time_steps)
+    # pitches = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     # single_frequency_spectrum = [0.0, 0.1, 0.1, 0.5, 0.8, 0.7, 0.6, 0.5, 0.4, 0.7, 0.6, 0.4, 0.2, 0.1, 0.1, 0.0]
-    spectrum = np.empty(number_of_pitches, dtype=object)
-    for i in range(number_of_pitches):
-        single_frequency_spectrum = np.zeros(number_of_time_steps)
-        for j in range(number_of_time_steps):
-            single_frequency_spectrum[j] = random.uniform(0, 1)
-        spectrum[i] = single_frequency_spectrum
+    # spectrum = np.empty(number_of_pitches, dtype=object)
+    # for i in range(number_of_pitches):
+    #     single_frequency_spectrum = np.zeros(number_of_time_steps)
+    #     for j in range(number_of_time_steps):
+    #         single_frequency_spectrum[j] = random.uniform(0, 1)
+    #     spectrum[i] = single_frequency_spectrum
     # print(single_frequency_spectrum[len(single_frequency_spectrum)-1])
     # f = interp1d(t, single_frequency_spectrum)
     # x_new = np.linspace(0, 2.5, num=100, endpoint=True)
@@ -145,18 +145,29 @@ def main():
     # plot_note_graph(single_frequency_spectrum[len(single_frequency_spectrum)-1], t)
     # plot_note_graph(single_frequency_spectrum, t)
     # get_maxima_times(single_frequency_spectrum, t, 0.5, printing=True)
-    # print(get_all_maxima_times(spectrum, t, 0.7))
-    note_predictions = get_note_predictions(pitches, spectrum, t, 0.7)
-    print(note_predictions)
-    print()
+    # print(get_maxima_times_for_all_pitches(spectrum, t, 0.7))
+    # note_predictions = get_note_predictions(pitches, spectrum, t, 0.7)
+    # print(note_predictions)
+    # print()
     # sorted(note_predictions, key=lambda note_prediction: note_prediction[1][0])
     # print(note_predictions)
     # separated_notes = separate_notes(note_predictions)
-    separated_notes_sorted = get_sorted_notes(pitches, spectrum, t, 0.7)
-    print(separated_notes_sorted)
-    print()
+    # separated_notes_sorted = get_sorted_notes(pitches, spectrum, t, 0.7)
+    # print(separated_notes_sorted)
+    # print()
     model = load_model('baseline')
-    get_pitch_probabilities('A4', model, plotting=True)
+    spectrum, _, times, _ = get_spectrogram(f'wav_files/single_D4_0.wav')
+    print(spectrum.shape)
+    p = np.zeros(spectrum.shape[1])
+    a = np.zeros(spectrum.shape[1])
+    for i in range(spectrum.shape[1]):
+        periodogram = spectrum[:, i].reshape(1, 8193, 1)
+        pitch_probabilities = model.predict(periodogram)[0]
+        a[i] = np.argmax(pitch_probabilities)
+        print(pitch_probabilities)
+        p[i] = pitch_probabilities[10]
+    print()
+    print(a)
 
 
 if __name__ == "__main__":
