95ccffb073e84dcf25b10709bca6630e83019e84
brbb2 <brbb2@cam.ac.uk>
2020-02-13 13:19:48 +0000

    Include "--preserve-silence" option

--- midi_to_wav_converter.py
+++ midi_to_wav_converter.py
@@ -7,22 +7,25 @@ PATH_WAV = 'wav_files'
 
 def run_timidity(filename):
     filename, _ = os.path.splitext(filename)
-    command = '{}/timidity {}/{}.mid -Ow -o {}/{}.wav'.format(PATH_TIMIDITY, PATH_MID, filename, PATH_WAV, filename)
+    command = '{}/timidity {}/{}.mid -Ow --preserve-silence -o {}/{}.wav'\
+        .format(PATH_TIMIDITY, PATH_MID, filename, PATH_WAV, filename)
     os.system(command)
 
 
-def make_all_wavs():
-    for filename in os.listdir(PATH_MID):
-        run_timidity(filename)
+def make_all_wavs(skipping_rest_files=False):
+    if skipping_rest_files:
+        for filename in os.listdir(PATH_MID):
+            if 'rest' in filename:
+                continue
+            else:
+                run_timidity(filename)
+    else:
+        for filename in os.listdir(PATH_MID):
+            run_timidity(filename)
 
 
 def main():
-    for filename in os.listdir(PATH_MID):
-        if 'rest' in filename:
-            continue
-        else:
-            run_timidity(filename)
-    # make_all_wavs()
+    make_all_wavs()
 
 
 if __name__ == '__main__':
