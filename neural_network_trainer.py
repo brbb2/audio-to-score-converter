02e15ff47875996545191950fe2db60067ae0aaa
brbb2 <brbb2@cam.ac.uk>
2020-02-15 23:58:34 +0000

    Train with different window sizes

--- neural_network_trainer.py
+++ neural_network_trainer.py
@@ -12,15 +12,6 @@ from keras.models import model_from_json
 from keras.callbacks import TensorBoard, EarlyStopping
 
 
-def print_normalisations(x):
-    print('x:')
-    print(x)
-    print('\nnormalize(x, axis=0):')
-    print(normalize(x, axis=0))
-    print('\nnormalize(x, axis=1):')
-    print(normalize(x, axis=1))
-
-
 def print_shapes(x, y, x_train, y_train, x_test, y_test):
     print(f'      x.shape: {str(x.shape): >12}          y.shape: {str(y.shape): >7}')
     print(f'x_train.shape: {str(x_train.shape): >12}    y_train.shape: {str(y_train.shape): >7}')
@@ -52,21 +43,56 @@ def print_counts_table(y, y_train, y_test):
         print(f'{str(y_targets[i]): <9} | {y_counts[i]: >7}  {y_train_count: >7}  {y_test_count: >7}')
 
 
-def get_data(encoding='midi_pitch', midi=False):
+def print_normalisation_options(x):
+    print('x:')
+    print(x)
+    print('\nkeras.utils.normalize(x, axis=0):')
+    print(normalize(x, axis=0))
+    print('\nkeras.utils.normalize(x, axis=1):')
+    print(normalize(x, axis=1))
+
+
+def normalise_x(x, axis=0, printing=False, printing_normalisation_options=False):
+    x_normalised = normalize(x, axis=axis)
+    if printing:
+        print()
+        print(x_normalised)
+    if printing_normalisation_options:
+        print_normalisation_options(x)
+    return x_normalised
+
+
+def normalise_x_train_and_x_val(x_train, x_val, axis=0, printing=False):
+    separation_index = x_train.shape[axis]
+    x = np.concatenate((x_train, x_val), axis=axis)
+    x_normalised = normalize(x, axis=axis)
+    x_train_normalised = x_normalised[:separation_index]
+    x_val_normalised = x_normalised[separation_index:]
+    if printing:
+        print()
+        print(f'           x_train.shape: {x_train.shape}    '
+              f'             x_val.shape: {x_val.shape}\n'
+              f'x_train_normalised.shape: {x_train_normalised.shape}    '
+              f'x_train_normalised.shape: {x_val_normalised.shape}')
+        print()
+        print(x_normalised)
+    return x_train_normalised, x_val_normalised
+
+
+def get_data(encoding='midi_pitch', midi_bins=False, nperseg=4096, noverlap=2048):
     x_list = list()
     y_list = list()
 
     # for each wav file in the data directory
     for filename in os.listdir('wav_files'):
         # get the spectrogram of the audio file
-        f, t, sxx = get_spectrogram_scipy(f'wav_files/{filename}', midi=midi)
-        # print(f'len(frequencies): {len(frequencies)}')
-        # print(spectrum.shape, len(t))
+        f, t, sxx = get_spectrogram_scipy(f'wav_files/{filename}', midi_bins=midi_bins,
+                                          nperseg=nperseg, noverlap=noverlap)
         # and get the ground-truth note for each periodogram in the spectrum
         filename, _ = os.path.splitext(filename)  # remove file extension from the filename
         ground_truth = get_monophonic_ground_truth(f'wav_files/{filename}.wav',
                                                    f'xml_files/{filename}.musicxml',
-                                                   encoding=encoding)
+                                                   encoding=encoding, nperseg=nperseg, noverlap=noverlap)
         # add each periodogram and its corresponding note to x_list and y_list respectively
         for i in range(len(ground_truth)):
             x_list.insert(0, sxx[:, i])
@@ -105,6 +131,7 @@ def split_data(x, y, n_splits=1, test_size=0.1, random_state=42, printing=False)
         y_train, y_test = y[train_indices], y[test_indices]
 
     if printing:
+        print()
         print_shapes(x, y, x_train, y_train, x_test, y_test)
         print()
         print_counts_table(y, y_train, y_test)
@@ -112,44 +139,70 @@ def split_data(x, y, n_splits=1, test_size=0.1, random_state=42, printing=False)
     return x_train, y_train, x_test, y_test
 
 
-def get_and_split_data(encoding='midi_pitch', n_splits=1, test_size=0.1, random_state=42,
-                       midi=False, normalising=True, printing=False):
-    x, y = get_data(encoding=encoding, midi=midi)
-    if normalising:
-        x = normalize(x, axis=0)
+def preprocess_data(x_train, y_train, x_val, y_val, encoding='midi_pitch',
+                    normalising_features=True, axis=0, start=21, printing=False):
+
+    if normalising_features:
+        x_train, x_val = normalise_x_train_and_x_val(x_train, x_val, axis=axis)
+
     if printing:
-        print_normalisations(x)
-    return split_data(x, y, n_splits=n_splits, test_size=test_size, random_state=random_state, printing=printing)
+        x_train_original = np.array(x_train)
+        y_train_original = np.array(y_train)
+        x_val_original = np.array(x_val)
+        y_val_original = np.array(y_val)
 
+    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+    x_val = x_val.reshape(x_val.shape[0], x_val.shape[1], 1)
 
-def preprocess_data(x_train, y_train, x_val, y_val, printing=False):
-
-    x_train_reshaped = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
-
-    y_train = y_train - 20
-    mask_train = np.where(y_train == -21)
+    y_train = y_train - start + 1
+    mask_train = np.where(y_train == midi_manager.REST_ENCODING - start + 1)
     y_train[mask_train] = 0
-    y_train_one_hot = to_categorical(y_train, num_classes=89, dtype='float32')
 
-    x_val_reshaped = x_val.reshape(x_val.shape[0], x_val.shape[1], 1)
-
-    y_val = y_val - 20
-    mask_val = np.where(y_val == -21)
+    y_val = y_val - start + 1
+    mask_val = np.where(y_val == midi_manager.REST_ENCODING - start + 1)
     y_val[mask_val] = 0
-    y_val_one_hot = to_categorical(y_val, num_classes=89, dtype='float32')
+
+    if encoding == 'one_hot':
+        y_train = to_categorical(y_train, num_classes=89, dtype='float32')
+        y_val = to_categorical(y_val, num_classes=89, dtype='float32')
 
     if printing:
+        print(x_train_original)
         print(x_train)
-        print(x_train_reshaped)
+        print(y_train_original)
         print(y_train)
-        print(y_train_one_hot)
         print()
-        print(f'         x_train.shape: {x_train.shape}')
-        print(f'x_train_reshaped.shape: {x_train_reshaped.shape}')
-        print(f'         y_train.shape: {y_train.shape}')
-        print(f' y_train_one_hot.shape: {y_train_one_hot.shape}\n')
+        print(f'         x_train.shape: {x_train_original.shape}')
+        print(f'x_train_reshaped.shape: {x_train.shape}')
+        print(f'         y_train.shape: {y_train_original.shape}')
+        print(f' y_train_one_hot.shape: {y_train.shape}\n')
+        print()
+        print(x_val_original)
+        print(x_val)
+        print(y_val_original)
+        print(y_val)
+        print()
+        print(f'           x_val.shape: {x_val_original.shape}')
+        print(f'  x_val_reshaped.shape: {x_val.shape}')
+        print(f'           y_val.shape: {y_val_original.shape}')
+        print(f'   y_val_one_hot.shape: {y_val.shape}\n')
 
-    return x_train_reshaped, y_train_one_hot, x_val_reshaped, y_val_one_hot
+    return x_train, y_train, x_val, y_val
+
+
+def get_and_prepare_data(encoding='midi_pitch', n_splits=1, test_size=0.1, random_state=42,
+                         nperseg=4096, noverlap=2048, midi_bins=False, normalising_features=True,
+                         printing=False, saving=False, version=None):
+    x, y = get_data(encoding=encoding, midi_bins=midi_bins, nperseg=nperseg, noverlap=noverlap)
+    x_train, y_train, x_val, y_val = split_data(x, y, n_splits=n_splits, test_size=test_size,
+                                                random_state=random_state, printing=printing)
+    x_train, y_train, x_val, y_val = preprocess_data(x_train, y_train, x_val, y_val,
+                                                     normalising_features=normalising_features)
+
+    if saving and version is not None:
+        save_data_arrays(x_train, y_train, x_val, y_val, version)
+
+    return x_train, y_train, x_val, y_val
 
 
 def print_data(x_train, y_train, x_val, y_val):
@@ -163,18 +216,32 @@ def print_data(x_train, y_train, x_val, y_val):
     print(y_val)
 
 
-def save_data_arrays(x_train, y_train, x_val, y_val, version):
-    np.save(f'data_arrays/version{version}/x_train.npy', x_train)
-    np.save(f'data_arrays/version{version}/y_train.npy', y_train)
-    np.save(f'data_arrays/version{version}/x_val.npy', x_val)
-    np.save(f'data_arrays/version{version}/y_val.npy', y_val)
+def save_data_arrays(x_train, y_train, x_val, y_val, version, printing=True):
+
+    path = f'data_arrays/{version}'
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print(f'Creation of the directory \"{path}\" failed.')
+    else:
+        if printing:
+            print(f'Successfully created the directory \"{path}\".')
+
+    np.save(f'data_arrays/{version}/x_train.npy', x_train)
+    np.save(f'data_arrays/{version}/y_train.npy', y_train)
+    np.save(f'data_arrays/{version}/x_val.npy', x_val)
+    np.save(f'data_arrays/{version}/y_val.npy', y_val)
+
+    if printing:
+        print(f'Successfully saved data arrays in the directory \"{path}\".')
 
 
 def load_data_arrays(version):
-    x_train = np.load(f'data_arrays/version{version}/x_train.npy')
-    y_train = np.load(f'data_arrays/version{version}/y_train.npy')
-    x_val = np.load(f'data_arrays/version{version}/x_val.npy')
-    y_val = np.load(f'data_arrays/version{version}/y_val.npy')
+    x_train = np.load(f'data_arrays/{version}/x_train.npy')
+    y_train = np.load(f'data_arrays/{version}/y_train.npy')
+    x_val = np.load(f'data_arrays/{version}/x_val.npy')
+    y_val = np.load(f'data_arrays/{version}/y_val.npy')
     return x_train, y_train, x_val, y_val
 
 
@@ -182,6 +249,10 @@ def get_model_definition(model_name, x_shape, printing=False):
     model = None
     if model_name == 'baseline':
         model = get_model_baseline(x_shape, printing=printing)
+    elif model_name == 'new':
+        model = get_model_new(x_shape, printing=printing)
+    elif model_name == 'midi':
+        model = get_model_midi(x_shape, printing=printing)
     elif model_name == 'baseline3':
         model = get_model_3(x_shape, printing=printing)
     elif model_name == 'baseline4':
@@ -192,10 +263,6 @@ def get_model_definition(model_name, x_shape, printing=False):
         model = get_model_6(x_shape, printing=printing)
     elif model_name == 'baseline7':
         model = get_model_7(x_shape, printing=printing)
-    elif model_name == 'new':
-        model = get_model_new(x_shape, printing=printing)
-    elif model_name == 'midi':
-        model = get_model_midi(x_shape, printing=printing)
 
     return model
 
@@ -258,11 +325,12 @@ def load_model_and_get_predictions(model_name, x):
     return model.predict([x])
 
 
-def show_example_prediction(i=0, version=1, x_val=None, y_val=None, printing_in_full=False):
+def show_example_prediction(model_name, i=0, version=1, x_val=None, y_val=None, printing_in_full=False):
     if x_val is None or y_val is None:
         _, _, x_val, y_val = load_data_arrays(version)
     example = (x_val[i], y_val[i])
-    example_prediction = load_model_and_get_predictions('new3', [example[0]])[0]
+    model_input = example[0].reshape(1, example[0].shape[0], 1)
+    example_prediction = load_model_and_get_predictions(model_name, model_input)[0]
     example_ground_truth = example[1]
     i_prediction = np.argmax(example_prediction)
     i_ground_truth = np.argmax(example_ground_truth)
@@ -272,9 +340,11 @@ def show_example_prediction(i=0, version=1, x_val=None, y_val=None, printing_in_
     note_name_ground_truth = midi_manager.get_note_name(midi_pitch_ground_truth)
 
     if printing_in_full:
-        print()
+        print(f'\nmodel_input: {model_input.shape}')
+        print(model_input)
+        print(f'\nexample_prediction: {example_prediction.shape}')
         print(example_prediction)
-        print()
+        print(f'\nexample_ground_truth: {example_ground_truth.shape}')
         print(example_ground_truth)
 
     print()
@@ -284,12 +354,12 @@ def show_example_prediction(i=0, version=1, x_val=None, y_val=None, printing_in_
     print(f' note_name_prediction: {note_name_prediction:>4}    note_name_ground_truth: {note_name_ground_truth:>4}')
 
 
-def show_first_n_predictions(n=25, x_val=None, y_val=None, version=1):
+def show_first_n_predictions(model_name, n=25, x_val=None, y_val=None, version=1, printing_in_full=False):
     if x_val is None or y_val is None:
         _, _, x_val, y_val = load_data_arrays(version)
     assert (n < len(x_val))
     for i in range(n):
-        show_example_prediction(i, x_val=x_val, y_val=y_val)
+        show_example_prediction(model_name, i, x_val=x_val, y_val=y_val, printing_in_full=printing_in_full)
 
 
 def evaluate(model, x_test, y_test, printing=True):
@@ -347,16 +417,15 @@ def cross_validate(model, model_name, x_dev, y_dev, n_folds=10, shuffling=True):
 
 
 def main():
-    # x_train, y_train, x_val, y_val = get_and_split_data(midi=True)
-    # x_train, y_train, x_val, y_val = preprocess_data(x_train, y_train, x_val, y_val)
-    # save_arrays(x_train, y_train, x_val, y_val, 2)
+    x_train, y_train, x_val, y_val = get_and_prepare_data(midi_bins=False, nperseg=2048, noverlap=1024,
+                                                          saving=True, version='label_freq_025ms')
+    x_train, y_train, x_val, y_val = load_data_arrays('label_freq_025ms')
+    print_data(x_train, y_train, x_val, y_val)
 
-    x_train, y_train, x_val, y_val = load_data_arrays(1)
-    # print_data(x_train, y_train, x_val, y_val)
+    train('new', 'label_freq_025ms', x_train, y_train, x_val, y_val, printing=True,
+          loss='sparse_categorical_crossentropy')
 
-    # train('midi', 'new4', x_train, y_train, x_val, y_val, printing=True)
-
-    show_first_n_predictions(x_val=x_val, y_val=y_val)
+    # show_example_prediction('one_hot_freq_050ms', 2, x_val=x_val, y_val=y_val, printing_in_full=True)
 
 
 if __name__ == "__main__":
