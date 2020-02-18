2d40171c6ddf532e6851cf6f638466ee98eed62a
brbb2 <brbb2@cam.ac.uk>
2020-02-18 00:03:29 +0000

    Clean up control flow

--- ground_truth_converter.py
+++ ground_truth_converter.py
@@ -1,9 +1,7 @@
 import numpy as np
 from music21 import *
-from audio_processor import get_spectrogram
-
-
-REST_ENCODING = -1
+from audio_processor import get_spectrogram_scipy
+from midi_manager import REST_ENCODING, one_hot_encode_midi_pitch
 
 
 def print_note(note_or_rest):
@@ -13,15 +11,23 @@ def print_note(note_or_rest):
         print(f'{note_or_rest.offset: >6}  Rest')
 
 
-def encode_midi_pitch(midi_pitch, start=21, end=108):
-    pitch_range = end - start + 2
-    one_hot_encoding = np.zeros(pitch_range)
-    if midi_pitch == REST_ENCODING:
-        one_hot_encoding[pitch_range-1] = 1
-    else:
-        i = pitch_range - midi_pitch + start - 2
-        one_hot_encoding[i] = 1
-    return one_hot_encoding
+def get_encoded_pitch(element, encoding='one_hot'):
+    encoded_pitch = None
+    if type(element) is note.Note:
+        if encoding == 'one_hot':
+            encoded_pitch = one_hot_encode_midi_pitch(element.pitch.midi)
+        elif encoding == 'midi_pitch':
+            encoded_pitch = element.pitch.midi
+        else:
+            encoded_pitch = str(element.pitch)
+    elif type(element) is note.Rest:
+        if encoding == 'one_hot':
+            encoded_pitch = one_hot_encode_midi_pitch(REST_ENCODING)
+        elif encoding == 'midi_pitch':
+            encoded_pitch = REST_ENCODING
+        else:
+            encoded_pitch = 'Rest'
+    return encoded_pitch
 
 
 def get_notes(score, printing=False, encoding='one_hot'):
@@ -29,42 +35,23 @@ def get_notes(score, printing=False, encoding='one_hot'):
     for part in score.getElementsByClass("Part"):
         for measure in part.getElementsByClass("Measure"):
             seconds_map = measure.secondsMap
+            if printing:
+                print('-- new measure --')
             for i in seconds_map:
-                # print(i)
                 element = i.get('element')
-                if type(element) is note.Note:
+                if type(element) == note.Note or type(element) == note.Rest:
+                    encoded_pitch = get_encoded_pitch(element, encoding=encoding)
                     offset_seconds = i.get('offsetSeconds')
                     duration_seconds = i.get('durationSeconds')
-                    if encoding == 'one_hot':
-                        one_hot_encoding = encode_midi_pitch(element.pitch.midi)
-                        notes.append((one_hot_encoding, element.offset, element.duration.quarterLength,
-                                      offset_seconds, duration_seconds))
-                    elif encoding == 'midi_pitch':
-                        notes.append((element.pitch.midi, element.offset, element.duration.quarterLength,
-                                      offset_seconds, duration_seconds))
-                    else:
-                        notes.append((str(element.pitch), element.offset, element.duration.quarterLength,
-                                      offset_seconds, duration_seconds))
+                    notes.append((encoded_pitch, element.offset, element.duration.quarterLength,
+                                  offset_seconds, duration_seconds))
+
                     if printing:
-                        print(f'note: {str(element.pitch): <4}  '
-                              f'offset: {element.offset}  '
-                              f'offsetSeconds: {offset_seconds}')
-                if type(element) is note.Rest:
-                    offset_seconds = i.get('offsetSeconds')
-                    duration_seconds = i.get('durationSeconds')
-                    if encoding == 'one_hot':
-                        notes.append((encode_midi_pitch(REST_ENCODING), element.offset, element.duration.quarterLength,
-                                      offset_seconds, duration_seconds))
-                    elif encoding == 'midi_pitch':
-                        notes.append((REST_ENCODING, element.offset, element.duration.quarterLength,
-                                      offset_seconds, duration_seconds))
-                    else:
-                        notes.append(('Rest', element.offset, element.duration.quarterLength,
-                                      offset_seconds, duration_seconds))
-                    if printing:
-                        print(f'note: Rest  '
-                              f'offset: {element.offset}  '
-                              f'offsetSeconds: {offset_seconds}')
+                        print(f'note: {str(encoded_pitch): <4}    '
+                              f'offset: {float(element.offset):7.4f} beats ({float(offset_seconds):7.4f} s)    '
+                              f'duration: {float(element.duration.quarterLength):7.4f} beats ('
+                              f'{float(duration_seconds):7.4f} s)')
+
     return notes
 
 
@@ -73,7 +60,7 @@ def get_monophonic_periodogram_note(times, notes, encoding='one_hot'):
     if encoding == 'one_hot':
         ground_truth = np.empty(len(times), dtype=object)
         for i in range(len(ground_truth)):
-            ground_truth[i] = encode_midi_pitch(REST_ENCODING)
+            ground_truth[i] = one_hot_encode_midi_pitch(REST_ENCODING)
     elif encoding == 'midi_pitch':
         ground_truth = np.full(len(times), REST_ENCODING, dtype=object)
     else:
@@ -87,8 +74,8 @@ def get_monophonic_periodogram_note(times, notes, encoding='one_hot'):
     return ground_truth
 
 
-def get_monophonic_ground_truth(wav, xml, encoding='one_hot', printing=False):
-    _, _, times, _ = get_spectrogram(wav)
+def get_monophonic_ground_truth(wav, xml, encoding='one_hot', nperseg=4096, noverlap=2048, printing=False):
+    _, times, _ = get_spectrogram_scipy(wav, nperseg=nperseg, noverlap=noverlap)
     score = converter.parse(xml)
     notes = get_notes(score, encoding=encoding)
     ground_truth = get_monophonic_periodogram_note(times, notes, encoding)
@@ -105,6 +92,7 @@ def run_test_case(encoding=None):
 
 def main():
     run_test_case(encoding=None)
+    run_test_case(encoding='midi_pitch')
 
 
 if __name__ == "__main__":
