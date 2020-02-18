b58de6a7e7d6d535d845e96d66d2db85b2351743
brbb2 <brbb2@cam.ac.uk>
2020-02-18 00:01:09 +0000

    Modify 'print_note_info' function

--- data_generator.py
+++ data_generator.py
@@ -1,22 +1,21 @@
 from music21 import *
 import random
 import os
+from midi_manager import pitch_offset_names as pitch_values
+from midi_to_wav_converter import make_all_wavs
 
 
-pitch_values = {
-    0: 'C',
-    1: 'C#',
-    2: 'D',
-    3: 'E-',
-    4: 'E',
-    5: 'F',
-    6: 'F#',
-    7: 'G',
-    8: 'A-',
-    9: 'A',
-    10: 'B-',
-    11: 'B'
-}
+def print_note_info(input_note):
+    if type(input_note) is note.Note:
+        print(f'{input_note.name:<2}  '
+              f'{input_note.octave:>2}  '
+              + '{:<8}'.format(str(input_note.duration.quarterLength)) + ' '
+              f'{input_note.duration.type:<10} '
+              f'{input_note.volume.velocity:>3}')
+    elif type(input_note) is note.Rest:
+        print(f'Rest    '
+              + '{:<8}'.format(str(input_note.duration.quarterLength)) + ' '
+              f'{input_note.duration.type:<10} ')
 
 
 def get_random_length():
@@ -47,53 +46,6 @@ def get_single_note_stream(note_pitch, note_octave, note_length, note_velocity,
     return s
 
 
-def print_note_info(input_note):
-    print(f'{input_note.octave: >2} '
-          f'{input_note.name: <2} '
-          # f'{note_length: >8.4f} '
-          + '{: <8}'.format(str(input_note.duration.quarterLength)) + ' '
-          f'{input_note.duration.type: <10} '
-          f'{input_note.volume.velocity: >3}')
-
-
-def generate_wav(s, file_path, sound_font):
-    s_midi = s.write('midi', f'{file_path}.mid')
-
-    # fs = fluidsynth
-    print(s_midi)
-    # fs.play_midi('Scarborough Fair.mid')
-    # print(f'FluidSynth(\'{sound_font}\', sample_rate=44100)')
-    # print(f'fs.midi_to_audio(\'{s_midi}\', \'{file_path}.wav\')')
-    s_wav = None
-    # s_wav = fs.midi_to_audio(f'{file_path}.mid', f'{file_path}.wav')
-    # s_wav = fs.midi_to_audio(s_midi, f'{file_path}.wav')
-    return s_wav
-
-
-def generate_midi_single_notes(samples=10, octaves=9, pitches=12, printing=False):
-    for octave in range(octaves):
-        for note_pitch in range(pitches):
-            if octave < 1 and note_pitch < 9:
-                continue  # enforce that no pitch is below A0
-            if octave >= 8 and note_pitch > 0:
-                break  # enforce that no pitch is above C8
-            for i in range(samples):
-                note_length = get_random_length()
-                s = get_single_note_stream(note_pitch, octave, note_length, random.randint(1, 128))
-                filename = f'single_{pitch_values[note_pitch]}{octave}_{i}'
-                s.write('midi', f'midi_files/{filename}.mid')
-                if printing:
-                    print(f'{filename}.mid')
-
-
-def get_xml_from_midi():
-    for filename in os.listdir('midi_files'):
-        filename, _ = os.path.splitext(filename)
-        print(filename)
-        s = converter.parse(f'midi_files/{filename}.mid')
-        s.write('musicxml', f'xml_files/{filename}.musicxml')
-
-
 def generate_midi_rests(samples=10, printing=False):
     for i in range(samples):
         s = get_rest_stream()
@@ -103,64 +55,45 @@ def generate_midi_rests(samples=10, printing=False):
             print(f'{filename}.mid')
 
 
-def generate_data(saving_wav=True, saving_xml=True, printing=False):
-    for octave in range(8):
-        for note_pitch in range(12):
+def generate_midi_single_notes(samples=10, start=0, octaves=9, pitches=12, printing=False):
+    for octave in range(octaves):
+        for note_pitch in range(pitches):
             if octave < 1 and note_pitch < 9:
-                continue
-            for i in range(10):
+                continue  # enforce that no pitch is below A0
+            if octave >= 8 and note_pitch > 0:
+                break  # enforce that no pitch is above C8
+            for i in range(samples):
                 note_length = get_random_length()
                 s = get_single_note_stream(note_pitch, octave, note_length, random.randint(1, 128))
-                filename = f'single_{pitch_values[note_pitch]}{octave}_{i}'
+                filename = f'single_{pitch_values[note_pitch]}{octave}_{start+i}'
                 s.write('midi', f'midi_files/{filename}.mid')
-                # if saving_wav:
-                #     generate_wav(s, f'wav-files/{filename}', 'FluidR3_GM.sf2')
-                # else:
-                #     s.write('midi', f'midi_files/{filename}.mid')
-                # if saving_xml:
-                #     s.write(fp=f'xml-files/{filename}.xml')
-                # if printing:
-                #     print_note_info(s[1])
-                print(filename)
-                # print(octave, note_pitch, i+1)
+                if printing:
+                    print(f'{filename}.mid')
 
 
-def example():
-    n = note.Note('C4')
-    print(f'                      n = note.Note(\'C4\')')
-    print(f'                 n.name = {n.name}')
-    print(f'               n.octave = {n.octave}')
-    print(f'       n.nameWithOctave = {n.nameWithOctave}')
-    print(f'           n.pitch.midi = {n.pitch.midi}')
-    print(f'n.pitch.diatonicNoteNum = {n.pitch.diatonicNoteNum}')
-    print(f'     n.pitch.pitchClass = {n.pitch.pitchClass}')
-    print(f'      n.pitch.frequency = {n.pitch.frequency:.2f} Hz')
+def generate_wav_files_from_all_midi_files():
+    make_all_wavs()
 
 
-def example_note(note_name):
-    n = note.Note(note_name)
-    print(f'                      n = note.Note(\'{note_name}\')')
-    print(f'                 n.name = {n.name}')
-    print(f'               n.octave = {n.octave}')
-    print(f'       n.nameWithOctave = {n.nameWithOctave}')
-    print(f'           n.pitch.midi = {n.pitch.midi}')
-    print(f'n.pitch.diatonicNoteNum = {n.pitch.diatonicNoteNum}')
-    print(f'     n.pitch.pitchClass = {n.pitch.pitchClass}')
-    print(f'      n.pitch.frequency = {n.pitch.frequency:.2f} Hz')
+def generate_xml_files_from_all_midi_files():
+    for filename in os.listdir('midi_files'):
+        filename, _ = os.path.splitext(filename)
+        print(filename)
+        s = converter.parse(f'midi_files/{filename}.mid')
+        s.write('musicxml', f'xml_files/{filename}.musicxml')
 
 
 def main():
-    s = get_single_note_stream(0, 4, 2, 100, 1, 5)
-    # print_note_info(s[1])
-    # s.show()
     # print(f'scratch-xml-files/{pitch_values[s[1].pitch.pitchClass]}{s[1].octave}.xml')
     # print(pitch_values[6])
     # example()
     # generate_wav(s, 'midi_files/demo', 'soundfonts/FluidR3_GM.sf2')
-    # generate_midi_single_notes(printing=True)
+    # generate_midi_single_notes(samples=2, start=12, printing=True)
     # generate_midi_rests()
     # example_note('C8')
-    get_xml_from_midi()
+    # get_xml_from_midi()
+    # generate_midi_rests(samples=1)
+    pass
 
 
 if __name__ == "__main__":
