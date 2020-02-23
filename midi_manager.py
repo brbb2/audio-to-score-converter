b7bd95330a5f62f89ac94994b10e37b079419811
brbb2 <brbb2@cam.ac.uk>
2020-02-23 21:09:48 +0000

    Vectorize encoding functions for operating on arrays

--- midi_manager.py
+++ midi_manager.py
@@ -37,7 +37,7 @@ def get_pitch_array(start=21, end=108):
 
 
 def get_midi_pitch(note_name, printing=False):
-    if note_name == 'Rest':
+    if note_name == 'rest' or note_name == 'Rest':
         return REST_ENCODING
     if len(note_name) < 2 or 3 < len(note_name):
         print('error')
@@ -68,13 +68,24 @@ def get_midi_pitch(note_name, printing=False):
 
 def get_note_name(midi_pitch, start=21, end=108):
     if midi_pitch == REST_ENCODING:
-        return 'Rest'
+        return 'rest'
     if midi_pitch < start or end < midi_pitch:
         print('error')
     else:
-        pitch__name = pitch_offset_names[midi_pitch % 12]
+        pitch_name = pitch_offset_names[midi_pitch % 12]
         octave = midi_pitch // 12 - 1
-        return pitch__name + str(octave)
+        return pitch_name + str(octave)
+
+
+def decode_midi_pitch(midi_pitch, start=21, end=108):
+    if midi_pitch == REST_ENCODING:
+        return 'rest'
+    if midi_pitch < start or end < midi_pitch:
+        print('error')
+    else:
+        pitch_name = pitch_offset_names[midi_pitch % 12]
+        octave = midi_pitch // 12 - 1
+        return pitch_name + str(octave)
 
 
 def get_one_hot_midi_pitch_index(midi_pitch, start=21, end=108, putting_rests_last=False, low_last=False):
@@ -94,6 +105,27 @@ def get_one_hot_midi_pitch_index(midi_pitch, start=21, end=108, putting_rests_la
             i += 1  # if rests are to go at the start, shift any other pitch along by 1
 
     return i
+
+
+def label_encode_midi_pitch(midi_pitch, start=21, end=108, putting_rests_last=False, low_last=False):
+    number_of_pitches = end - start + 1
+
+    if putting_rests_last:
+        if midi_pitch == REST_ENCODING:
+            return number_of_pitches
+        else:
+            if low_last:
+                return number_of_pitches - midi_pitch + start - 1
+            else:
+                return midi_pitch - start
+    else:
+        if midi_pitch == REST_ENCODING:
+            return 0
+        else:
+            if low_last:
+                return number_of_pitches - midi_pitch + start
+            else:
+                return midi_pitch - start + 1
 
 
 def one_hot_encode_midi_pitch(midi_pitch, start=21, end=108, putting_rests_last=False, low_last=False):
@@ -136,8 +168,71 @@ def interpret_one_hot(array, encoding='midi_pitch', start=21, end=108, putting_r
         return midi_pitch
 
 
+def encode_note_name_array(ground_truth_array, encoding='midi_pitch', printing=False):
+    if encoding == 'one_hot':
+        encoded_ground_truth_array = np.empty(len(ground_truth_array), dtype=object)
+    else:
+        encoded_ground_truth_array = np.empty(len(ground_truth_array))
+    for i in range(len(ground_truth_array)):
+        midi_pitch = get_midi_pitch(ground_truth_array[i])
+        if encoding == 'midi_pitch':
+            encoded_ground_truth_array[i] = midi_pitch
+        elif encoding == 'label':
+            encoded_ground_truth_array[i] = label_encode_midi_pitch(midi_pitch)
+        elif encoding == 'one_hot':
+            encoded_ground_truth_array[i] = one_hot_encode_midi_pitch(midi_pitch)
+    if printing:
+        print(f'        ground_truth_array: {ground_truth_array}\n'
+              f'encoded_ground_truth_array: {encoded_ground_truth_array}')
+    return encoded_ground_truth_array
+
+
+def encode_ground_truth_array(ground_truth_array, current_encoding=None, desired_encoding='midi_pitch',
+                              printing=False):
+    if current_encoding is None:
+        midi_pitch_encoded_array = np.vectorize(get_midi_pitch)(ground_truth_array)
+        if desired_encoding is None:
+            return ground_truth_array
+        elif desired_encoding == 'midi_pitch':
+            return midi_pitch_encoded_array
+        elif desired_encoding == 'label':
+            return np.vectorize(label_encode_midi_pitch)(midi_pitch_encoded_array)
+        elif desired_encoding == 'one_hot':
+            return np.vectorize(one_hot_encode_midi_pitch, otypes=[object])(midi_pitch_encoded_array)
+    elif current_encoding == 'midi_pitch':
+        if desired_encoding is None:
+            return np.vectorize(get_note_name)(ground_truth_array)
+        elif desired_encoding == 'midi_pitch':
+            return ground_truth_array
+        elif desired_encoding == 'label':
+            return np.vectorize(label_encode_midi_pitch)(ground_truth_array)
+        elif desired_encoding == 'one_hot':
+            return np.vectorize(one_hot_encode_midi_pitch, otypes=[object])(ground_truth_array)
+    elif current_encoding == 'label':
+        if desired_encoding is None:
+            pass
+        elif desired_encoding == 'midi_pitch':
+            pass
+        elif desired_encoding == 'label':
+            return ground_truth_array
+        elif desired_encoding == 'one_hot':
+            pass
+    elif current_encoding == 'one_hot':
+        midi_pitch_encoded_array = np.vectorize(interpret_one_hot, otypes=int)(ground_truth_array)
+        if desired_encoding is None:
+            return np.vectorize(interpret_one_hot(encoding=None), otypes=int)(ground_truth_array)
+        elif desired_encoding == 'midi_pitch':
+            return midi_pitch_encoded_array
+        elif desired_encoding == 'label':
+            return np.vectorize(label_encode_midi_pitch)(midi_pitch_encoded_array)
+        elif desired_encoding == 'one_hot':
+            return ground_truth_array
+
+
 def main():
-    pass
+    test_array = np.array(['C4', 'rest', 'A-1', 'F#7'])
+    a = encode_ground_truth_array(test_array, current_encoding=None, desired_encoding='label', printing=True)
+    print(a)
 
 
 if __name__ == "__main__":
