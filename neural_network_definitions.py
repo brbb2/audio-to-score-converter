4b98a8cf1bb876bdd8d463295d54045140100864
brbb2 <brbb2@cam.ac.uk>
2020-03-05 01:20:05 +0000

    Add simple feed-forward models with dense layers

--- neural_network_definitions.py
+++ neural_network_definitions.py
@@ -139,17 +139,84 @@ def get_model_7(x_train_shape, printing=False):
     return model
 
 
+def get_model_freq_dense(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Flatten(input_shape=x_train_shape[1:]))
+    model.add(Dense(512))
+
+    model.add(Dense(256))
+
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def get_model_midi_dense(x_train_shape, printing=False):
+
+    model = Sequential()
+
+    model.add(Flatten(input_shape=x_train_shape[1:]))
+    model.add(Dense(89))
+
+    model.add(Dense(200))
+
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
 def get_model_new(x_train_shape, printing=False):
 
     model = Sequential()
 
-    model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:]))
+    # model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:]))
+    model.add(Conv1D(filters=128, kernel_size=64, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=8))
+
+    model.add(Conv1D(filters=128, kernel_size=32))
+    # model.add(Conv1D(filters=2, kernel_size=32))
     model.add(MaxPool1D(pool_size=4))
 
+    model.add(Conv1D(filters=128, kernel_size=8, input_shape=x_train_shape[1:]))
+    model.add(MaxPool1D(pool_size=4))
+
+    model.add(Flatten())
+    model.add(Dense(89, activation='softmax'))
+
+    if printing:
+        for layer in model.layers:
+            print(f'{layer.__class__.__name__}:\n{layer.input_shape} -> {layer.output_shape}\n')
+        print()
+
+    return model
+
+
+def get_model_new_dropout(x_train_shape, dropout_rate=0.2, printing=False):
+
+    model = Sequential()
+
+    model.add(Dropout(dropout_rate, input_shape=x_train_shape[1:]))
+    model.add(Conv1D(filters=8, kernel_size=4))
+    model.add(MaxPool1D(pool_size=4))
+
+    model.add(Dropout(dropout_rate))
     model.add(Conv1D(filters=8, kernel_size=32))
     model.add(MaxPool1D(pool_size=4))
 
     model.add(Flatten())
+    model.add(Dropout(dropout_rate))
     model.add(Dense(89, activation='softmax'))
 
     if printing:
@@ -164,10 +231,12 @@ def get_model_midi(x_train_shape, printing=False):
 
     model = Sequential()
 
-    model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:]))
+    # model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:]))
+    model.add(Conv1D(filters=128, kernel_size=32, input_shape=x_train_shape[1:]))
     model.add(MaxPool1D(pool_size=2))
 
-    model.add(Conv1D(filters=8, kernel_size=8))
+    # model.add(Conv1D(filters=8, kernel_size=8))
+    model.add(Conv1D(filters=128, kernel_size=8))
     model.add(MaxPool1D(pool_size=2))
 
     model.add(Flatten())
@@ -181,20 +250,20 @@ def get_model_midi(x_train_shape, printing=False):
     return model
 
 
-def get_model_midi_dropout(x_train_shape, dropout_rate=0.2, printing=False):
+def get_model_midi_dropout(x_train_shape, dropout_rate=0, printing=False):
 
     model = Sequential()
 
-    model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:]))
+    model.add(Conv1D(filters=8, kernel_size=4, input_shape=x_train_shape[1:], dropout_rate=dropout_rate))
     model.add(MaxPool1D(pool_size=2))
 
     model.add(Conv1D(filters=8, kernel_size=8))
     model.add(MaxPool1D(pool_size=2))
 
-    model.add(Dropout(dropout_rate))
+    # model.add(Dropout(dropout_rate))
 
     model.add(Flatten())
-    model.add(Dense(89, activation='softmax'))
+    model.add(Dense(89, activation='softmax', dropout_rate=dropout_rate))
 
     if printing:
         for layer in model.layers:
