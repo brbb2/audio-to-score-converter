4d2a5c462230b7d0592f5c188dfd4418a309715d
brbb2 <brbb2@cam.ac.uk>
2020-03-07 00:37:05 +0000

    Implement a unit test for the merging of frequency bins into MIDI bins

---
+++ audio_processor_test.py
@@ -0,0 +1,19 @@
+from audio_processor import *
+import unittest
+
+
+class TestAudioProcessor(unittest.TestCase):
+
+    def setUp(self):
+        self.test_wav = 'wav_files/single_A0_0.wav'
+
+    def test_merge_frequencies_output_shape(self):
+        frequencies, _, spectrogram = get_spectrogram(self.test_wav)
+        midi_spectrum = merge_frequency_bins_into_midi_bins(spectrogram, frequencies)
+        print(f'original spectrum shape: {str(spectrogram.shape):>10}')
+        print(f'    midi spectrum shape: {str(midi_spectrum.shape):>10}')
+        self.assertEqual(midi_spectrum.shape, (88, spectrogram.shape[1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
