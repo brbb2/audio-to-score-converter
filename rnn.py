8ab6ebe9595b2949247050204888748e9ae979d7
brbb2 <brbb2@cam.ac.uk>
2020-03-07 00:45:12 +0000

    Modify toy RNN to accommodate real data

--- rnn.py
+++ rnn.py
@@ -2,24 +2,35 @@ from keras.models import Model
 from keras.layers import Input, LSTM, Dense
 from keras.callbacks import TensorBoard, EarlyStopping
 import numpy as np
-from encoder import decode_label, get_number_of_unique_labels
-from neural_network_trainer import save_trained_model
+from encoder import decode_label, get_number_of_unique_labels, encode_ground_truth_array
+from encoder import get_bof_artificial_periodogram, get_eof_artificial_periodogram
+from encoder import BoF_LABEL_ENCODING, EoF_LABEL_ENCODING
+from neural_network_trainer import save_trained_model, load_model
+from data_processor import load_rnn_data_arrays, print_rnn_split_data
+from evaluator import create_comparison_text_file
 
 
-def set_up_sequence_to_sequence_model(encoder_input_data, decoder_input_data, decoder_target_data,
+def load_rnn_model(name):
+    encoder_model = load_model(f'{name}_encoder')
+    decoder_model = load_model(f'{name}_decoder')
+    return encoder_model, decoder_model
+
+
+def set_up_sequence_to_sequence_model(encoder_inputs_train, decoder_inputs_train, decoder_outputs_train,
+                                      encoder_inputs_val, decoder_inputs_val, decoder_outputs_val,
                                       latent_space_size=256, batch_size=None, epochs=500,
                                       printing=True, saving=False, save_name=None):
 
     number_of_unique_labels = get_number_of_unique_labels(for_rnn=True)
 
     # set up the encoder
-    encoder_inputs = Input(shape=(encoder_input_data.shape[1], encoder_input_data.shape[2]))
+    encoder_inputs = Input(shape=(encoder_inputs_train.shape[1], encoder_inputs_train.shape[2]))
     encoder = LSTM(latent_space_size, return_state=True)
     __, state_h, state_c = encoder(encoder_inputs)
     encoder_states = [state_h, state_c]
 
     # set up the decoder as an LSTM layer followed by a dense layer
-    decoder_inputs = Input(shape=(None, decoder_input_data.shape[2]))
+    decoder_inputs = Input(shape=(None, decoder_inputs_train.shape[2]))
     decoder_lstm = LSTM(latent_space_size, return_sequences=True, return_state=True)
     decoder_dense = Dense(number_of_unique_labels, activation='softmax')
 
@@ -33,11 +44,12 @@ def set_up_sequence_to_sequence_model(encoder_input_data, decoder_input_data, de
     if printing:
         model.summary()
 
-    # tensorboard = TensorBoard(log_dir=f'logs/{model_name}')
+    tensorboard = TensorBoard(log_dir=f'logs/{save_name}')
     es = EarlyStopping(patience=10, min_delta=0, restore_best_weights=True)
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-    model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-              batch_size=batch_size, epochs=epochs, callbacks=[es], validation_split=0.2)
+    model.fit([encoder_inputs_train, decoder_inputs_train], decoder_outputs_train,
+              batch_size=batch_size, epochs=epochs, callbacks=[tensorboard, es],
+              validation_data=([encoder_inputs_val, decoder_inputs_val], decoder_outputs_val))
 
     encoder_model = Model(encoder_inputs, encoder_states)
 
@@ -58,28 +70,31 @@ def set_up_sequence_to_sequence_model(encoder_input_data, decoder_input_data, de
 
 
 def decode_sequence(input_sequence, encoder_model, decoder_model, maximum_sequence_length):
-
-    number_of_blanks_required = maximum_sequence_length - 1 - input_sequence.shape[1]
-    bof = np.full(shape=(1, 1, input_sequence.shape[2]), fill_value=-0.5)
-    bof[:, :, ::2] = 0
-    eofs = np.full(shape=(1, number_of_blanks_required, input_sequence.shape[2]), fill_value=-1.0)
-    eofs[:, :, ::3] = 0
-    input_sequence = np.concatenate((bof, input_sequence, eofs), axis=1)
+    assert len(input_sequence) <= maximum_sequence_length
+    number_of_eofs_required = 1 + maximum_sequence_length - input_sequence.shape[0]
+    bof = get_bof_artificial_periodogram(input_sequence.shape[1]).reshape((1, input_sequence.shape[1]))
+    eof = get_eof_artificial_periodogram(input_sequence.shape[1]).reshape((1, input_sequence.shape[1]))
+    eofs = np.repeat(eof, number_of_eofs_required, axis=0)
+    print(f' bof: {bof.shape}\n{bof}\n')
+    print(f'input: {input_sequence.shape}\n{input_sequence}\n')
+    print(f'eofs: {eofs.shape}\n{eofs}\n')
+    input_sequence = np.concatenate((bof, input_sequence, eofs), axis=0)
+    input_sequence = input_sequence.reshape((1, input_sequence.shape[0], input_sequence.shape[1]))
     print(f'input: {input_sequence.shape}\n{input_sequence}\n')
 
     # encode the input as state vectors
     states_value = encoder_model.predict(input_sequence)
-    print(states_value)
-    print()
+    # print(states_value)
+    # print()
 
     # initialise the current label to the beginning-of-file label
-    current_label = 89
+    current_label = BoF_LABEL_ENCODING
 
     # initialise the decoded sequence as an empty list
     decoded_sequence = list()
 
     # get the labels for end-of-file markers and blank markers
-    eof_label = 90
+    eof_label = EoF_LABEL_ENCODING
 
     # while there are still predictions to be made
     while len(decoded_sequence) < maximum_sequence_length and current_label is not eof_label:
@@ -105,122 +120,58 @@ def decode_sequence(input_sequence, encoder_model, decoder_model, maximum_sequen
 
 
 def main():
-    x = np.array([[[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4],
-                   [0, 0, 0, 0], [0, 0, 0, 0], [0.0, -1.0, -1.0, 0.0], [0.0, -1.0, -1.0, 0.0],
-                   [0.0, -1.0, -1.0, 0.0]],
-                  [[0.0, -0.5, 0.0, -0.5], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                   [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82], [0.82, 0.82, 0.82, 0.82],
-                   [0.82, 0.82, 0.82, 0.82], [0.9, 0.9, 0.9, 0.9]]
-                  ])
+    encoder_inputs_train, decoder_inputs_train, decoder_outputs_train,\
+        encoder_inputs_val, decoder_inputs_val, decoder_outputs_val = \
+        load_rnn_data_arrays('rnn_label_freq_050ms_powers')
+    # print_rnn_split_data(encoder_inputs_train, decoder_inputs_train, decoder_outputs_train,
+    #                      encoder_inputs_val, decoder_inputs_val, decoder_outputs_val)
 
-    y = np.array([[89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-                  [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90]])
+    # for i in range(2):
+    #     file = encoder_inputs_train[i]
+    #     # for periodogram in file:
+    #     #     print(periodogram)
+    #     print(file[60])
+    #     print()
+    # set_up_sequence_to_sequence_model(encoder_inputs_train, decoder_inputs_train, decoder_outputs_train,
+    #                                   encoder_inputs_val, decoder_inputs_val, decoder_outputs_val,
+    #                                   saving=True, save_name='rnn_label_freq_50_powers')
 
-    z = np.full(shape=y.shape, fill_value=91)
-    z[:, :-1] = y[:, 1:]
+    encoder_model, decoder_model = load_rnn_model('rnn_label_freq_50_powers')
 
-    # y = np.array([[89, 0, 0, 40, 40, 0, 0, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90],
-    #               [89, 0, 0, 40, 40, 0, 0, 90, 90, 90], [89, 0, 0, 0, 0, 82, 82, 82, 82, 90]])
-    #
-    # z = np.empty(len(y), dtype=object)
-    # for i in range(len(y)):
-    #     y[i] = np.array(y[i])
-    #     y[i] = y[i].reshape((y[i].shape[0], 1))
-    #
-    # for i in range(len(y)):
-    #     next_labels = np.full(shape=y[i].shape, fill_value=91)
-    #     next_labels[:-1, :] = y[i][1:, :]
-    #     z[i] = next_labels
+    sample = 54
+    file_name = f'validation_sample_{sample}'
 
-    print(f'x: {x.shape}\n{x}\n')
-    print(f'y: {y.shape}\n{y}\n')
-    print(f'z: {z.shape}\n{z}\n')
+    max_length = encoder_inputs_val.shape[1] - 2
+    ground_truth = decoder_inputs_val[sample][1:-1]
+    ground_truth = ground_truth.reshape(ground_truth.shape[0])
+    ground_truth = encode_ground_truth_array(ground_truth, current_encoding='label', desired_encoding=None)
+    ground_truth_list = list()
+    i = 0
+    while i < len(ground_truth) and ground_truth[i] != 'EoF':
+        ground_truth_list.insert(0, ground_truth[i])
+        i += 1
+    ground_truth = np.array(ground_truth_list)[::-1]
 
-    y = y.reshape((y.shape[0], y.shape[1], 1))
-    z = z.reshape((z.shape[0], z.shape[1], 1))
-
-    encoder_model, decoder_model = set_up_sequence_to_sequence_model(x, y, z)
-
-    test = np.array([[[0, 0, 0, 0], [0, 0, 0, 0], [0.4, 0.4, 0.4, 0.4], [0.4, 0.4, 0.4, 0.4], [0, 0, 0, 0],
-                      [0, 0, 0, 0]]])
-    ground_truth = np.array([0, 0, 40, 40, 0, 0])
-
-    print(f'test: {test.shape}\n{test}\n')
-
-    predicted_sequence = decode_sequence(test, encoder_model, decoder_model, 10)
+    predicted_sequence = decode_sequence(encoder_inputs_val[sample][1:-1], encoder_model, decoder_model, max_length)
 
     print(f'predicted sequence: {predicted_sequence.shape}\n{predicted_sequence}\n')
     print(f'ground truth: {ground_truth.shape}\n{ground_truth}\n')
+
+    f = open(f'txt_files/{file_name}.txt', 'w')
+    f.write('        time step:   ')
+    for time_step in range(max(len(predicted_sequence), len(ground_truth))):
+        f.write(f'{time_step:<5}')
+    f.write('\n')
+    f.write('     ground truth:   ')
+    for pitch in ground_truth:
+        if pitch == 'EoF':
+            break
+        f.write(f'{pitch:<5}')
+    f.write('\n')
+    f.write('model predictions:   ')
+    for pitch in predicted_sequence:
+        f.write(f'{pitch:<5}')
+    f.close()
 
 
 if __name__ == '__main__':
