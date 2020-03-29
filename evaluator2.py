db5adad144f28f11748c35538c2c8d8cdadea40e
brbb2 <brbb2@cam.ac.uk>
2020-03-29 20:08:53 +0100

    Replace accuracy calculations with F-measure

--- evaluator2.py
+++ evaluator2.py
@@ -4,6 +4,77 @@ from os.path import isfile, splitext
 from ground_truth_converter import get_ground_truth_notes
 
 
+def get_precision(ground_truth_notes, predicted_notes, printing=False):
+    number_of_predicted_notes_correct = 0
+
+    for predicted_note in predicted_notes:
+        predicted_note_name = predicted_note[0]
+        predicted_note_onset_time = predicted_note[1]
+        for ground_truth_note in ground_truth_notes:
+            ground_truth_note_name = ground_truth_note[0]
+            ground_truth_note_onset_time = ground_truth_note[1]
+            if ground_truth_note_name == predicted_note_name \
+                    and predicted_note_onset_time - 0.05 < ground_truth_note_onset_time \
+                    < predicted_note_onset_time + 0.05:
+                number_of_predicted_notes_correct += 1
+                break
+
+    precision = number_of_predicted_notes_correct / float(len(predicted_notes))
+
+    if printing:
+        print(f'\nnotes picked by system: {predicted_notes}')
+        print(f' notes in the XML file: {ground_truth_notes}')
+        print(f'\n             precision: {precision:6.4f}')
+
+    return precision
+
+
+def get_recall(ground_truth_notes, predicted_notes, printing=False):
+    number_of_correctly_found_notes = 0
+
+    for ground_truth_note in ground_truth_notes:
+        ground_truth_note_name = ground_truth_note[0]
+        ground_truth_note_onset_time = ground_truth_note[1]
+        for predicted_note in predicted_notes:
+            predicted_note_name = predicted_note[0]
+            predicted_note_onset_time = predicted_note[1]
+            if predicted_note_name == ground_truth_note_name \
+                    and ground_truth_note_onset_time - 0.05 < predicted_note_onset_time \
+                    < ground_truth_note_onset_time + 0.05:
+                number_of_correctly_found_notes += 1
+                break
+
+    recall = number_of_correctly_found_notes / float(len(ground_truth_notes))
+
+    if printing:
+        print(f'\nnotes picked by system: {predicted_notes}')
+        print(f' notes in the XML file: {ground_truth_notes}')
+        print(f'\n                recall: {recall:6.4f}')
+
+    return recall
+
+
+def get_f_score(ground_truth_notes, predicted_notes, precision=None, recall=None, beta=1, printing=False):
+    if precision is None:
+        precision = get_precision(ground_truth_notes, predicted_notes)
+    if recall is None:
+        recall = get_recall(ground_truth_notes, predicted_notes)
+
+    if precision == 0.0 and recall == 0.0:
+        f_score = 0.0
+    else:
+        f_score = (1 + beta) * ((precision * recall) / (beta * precision + recall))
+
+    if printing:
+        print(f'\nnotes picked by system: {predicted_notes}')
+        print(f' notes in the XML file: {ground_truth_notes}')
+        print(f'\n             precision: {precision:6.4f}')
+        print(f'\n                recall: {recall:6.4f}')
+        print(f'\n               F-score: {f_score:6.4f}')
+
+    return f_score
+
+
 def evaluate_prediction(file_name, threshold, wav_path='test_files/test_wavs', xml_path='test_files/test_xmls',
                         window_size=25, printing=False):
 
@@ -17,28 +88,11 @@ def evaluate_prediction(file_name, threshold, wav_path='test_files/test_wavs', x
     # use the pitches of all the windows to predict the notes
     predicted_notes = sweep(pitches_and_probabilities, quantising=True, threshold=threshold)
 
-    number_of_correctly_predicted_notes = 0
-
-    for ground_truth_note in ground_truth_notes:
-        ground_truth_note_name = ground_truth_note[0]
-        ground_truth_note_onset_time = ground_truth_note[1]
-        for predicted_note in predicted_notes:
-            predicted_note_name = predicted_note[0]
-            predicted_note_onset_time = predicted_note[1]
-            if predicted_note_name == ground_truth_note_name \
-                    and ground_truth_note_onset_time - 0.05 < predicted_note_onset_time \
-                    < ground_truth_note_onset_time + 0.05:
-                number_of_correctly_predicted_notes += 1
-                break
-
-    accuracy = number_of_correctly_predicted_notes / float(len(ground_truth_notes))
-
     if printing:
-        print(f'\nnotes picked by system: {predicted_notes}')
-        print(f' notes in the XML file: {ground_truth_notes}')
-        print(f'\n              accuracy: {accuracy:6.4f}')
+        print(f'{file_name}')
+    f_score = get_f_score(ground_truth_notes, predicted_notes, printing=printing)
 
-    return accuracy
+    return f_score
 
 
 def evaluate_all(test_file_names, threshold, wav_path='test_files/test_wavs', xml_path='test_files/test_xmls',
@@ -72,10 +126,12 @@ def brute_force_search_for_optimal_threshold_value(increment=0.01, start_thresho
     optimal_threshold_value_so_far = 0.0
     best_accuracy_so_far = 0.0
     threshold = start_threshold
+    if printing:
+        print(f'\n             threshold   F-score\n             -------------------')
     while threshold <= 1.0:
-        if printing:
-            print(threshold)
         accuracy_for_this_threshold_value = evaluate_all(test_files, threshold=threshold)
+        if printing:
+            print(f'             {threshold:10.8f}: {accuracy_for_this_threshold_value:6.4f}')
         if accuracy_for_this_threshold_value > best_accuracy_so_far:
             optimal_threshold_value_so_far = threshold
             best_accuracy_so_far = accuracy_for_this_threshold_value
@@ -88,9 +144,9 @@ def brute_force_search_for_optimal_threshold_value(increment=0.01, start_thresho
 
 def main():
     # evaluate_prediction('In the Bleak Midwinter', threshold=0.7, printing=True)
-    test_file_names = get_test_file_names()
-    evaluate_all(test_file_names, threshold=0.7, printing=True)
-    brute_force_search_for_optimal_threshold_value(start_threshold=0.5, increment=0.05, printing=True)
+    # test_file_names = get_test_file_names()
+    # evaluate_all(test_file_names, threshold=0.7, printing=True)
+    brute_force_search_for_optimal_threshold_value(start_threshold=0.95, increment=0.05, printing=True)
 
 
 if __name__ == '__main__':
